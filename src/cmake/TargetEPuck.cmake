c4d8ad0c18c3214d6107005041610936be5c2e2c
Alan Millard <millard.alan@gmail.com>
2016-10-29 17:06:26 +0100

    Modified build script for use with arm-linux-gnueabi cross-compilation toolchain.

--- src/cmake/TargetEPuck.cmake
+++ src/cmake/TargetEPuck.cmake
@@ -10,23 +10,23 @@
 set(CMAKE_SYSTEM_NAME Linux)
 
 # Full path to C compiler
-set(CMAKE_C_COMPILER /usr/local/angstrom/arm/bin/arm-angstrom-linux-gnueabi-gcc)
+set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabi-gcc)
 
 # Full path to C++ compiler
-set(CMAKE_CXX_COMPILER /usr/local/angstrom/arm/bin/arm-angstrom-linux-gnueabi-g++)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabi-g++)
 
 # Set the root directories for the find_* commands
 # Configure CMake to search for headers and libraries only in those directories
 set(CMAKE_FIND_ROOT_PATH
-  /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi
-  /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr/include
-  /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr/lib)
+  /usr/arm-linux-gnueabi
+  /usr/arm-linux-gnueabi/include
+  /usr/arm-linux-gnueabi/lib)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # By default, install stuff in the toolchain tree
-set(CMAKE_INSTALL_PREFIX /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr CACHE STRING "Install path prefix, prepended onto install directories.")
+set(CMAKE_INSTALL_PREFIX /usr/arm-linux-gnueabi/usr CACHE STRING "Install path prefix, prepended onto install directories.")
 
 # Compile with optimizations
 set(CMAKE_BUILD_TYPE Release CACHE STRING "Choose the type of build")
@@ -37,5 +37,5 @@ set(ARGOS_DOCUMENTATION OFF)
 set(ARGOS_DYNAMIC_LIBRARY_LOADING OFF)
 set(ARGOS_THREADSAFE_LOG OFF)
 set(ARGOS_USE_DOUBLE OFF)
-set(ARGOS_INCLUDE_DIRS /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr/include)
-set(ARGOS_LIBRARY_DIRS /usr/local/angstrom/arm/arm-angstrom-linux-gnueabi/usr/lib/argos3)
+set(ARGOS_INCLUDE_DIRS /usr/arm-linux-gnueabi/usr/include)
+set(ARGOS_LIBRARY_DIRS /usr/arm-linux-gnueabi/usr/lib/argos3)
