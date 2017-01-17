773c0416ceea6061830a57e63035f8840b98c6a0
Danesh Tarapore <daneshtarapore@gmail.com>
2017-01-17 21:44:45 +0000

    Update e-puck robot id to be set as last three digits of the robot's IP address

--- src/plugins/robots/e-puck/real_robot/real_epuck.cpp
+++ src/plugins/robots/e-puck/real_robot/real_epuck.cpp
@@ -20,6 +20,13 @@
 //#include <signal.h>
 #include <unistd.h>
 
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <stdlib.h>
+
 namespace argos {
 
 const std::string DEVICE = "/dev/ttyS2";
@@ -855,6 +862,49 @@ void CRealEPuck::SetId(CCI_Controller& c_controller) {
                << std::endl;
         c_controller.SetId("e-puck");
     }
+
+
+
+
+
+
+    struct ifaddrs *ifaddr, *ifa;
+    int s;
+    char ipaddress[NI_MAXHOST];
+
+    if (getifaddrs(&ifaddr) == -1)
+        THROW_ARGOSEXCEPTION("Error getifaddrs");
+
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        s=getnameinfo(ifa->ifa_addr, sizeof(struct sockaddr_in), ipaddress, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+
+        if((strcmp(ifa->ifa_name, "wlan0") == 0) && (ifa->ifa_addr->sa_family==AF_INET))
+        {
+            if (s != 0)
+            {
+                std::cerr << "getnameinfo() failed: " << gai_strerror(s);
+                LOGERR << "[WARNING] Failed to get hostname."
+                       << "Setting default robot id 'e-puck'."
+                       << std::endl;
+                c_controller.SetId("e-puck");
+            }
+            else
+            {
+                c_controller.SetId(std::string(pchBuffer).erase(0, 12));
+            }
+
+            //printf("\tInterface : <%s>\n",ifa->ifa_name );
+            //printf("\t  Address : <%s>\n", ipaddress);
+            break;
+        }
+    }
+
+    freeifaddrs(ifaddr);
 }
 
 /****************************************/
